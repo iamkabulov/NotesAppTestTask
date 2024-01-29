8747e4840b81e49043a2b1d5e699ba3f5ec58b8b
kabulov <n.kabulov@cft.ru>
2024-01-29 23:06:08 +0600

    Добавил viewController для заметки

---
+++ NotesAppTestTask/NotesAppTestTask/NoteModule/NoteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NoteViewController.swift
+//  NotesAppTestTask
+//
+//  Created by Nursultan Kabulov on 29.01.2024.
+//
+
+import UIKit
+
+final class NoteViewController: UIViewController
+{
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .darkGray
+		// Do any additional setup after loading the view.
+	}
+
+
+}
