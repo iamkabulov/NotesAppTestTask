d78aa45d5554c33f3fc983658f26354d5fd5aa18
kabulov <n.kabulov@cft.ru>
2024-01-29 20:28:39 +0600

    Создал проект без сториборда

---
+++ NotesAppTestTask/NotesAppTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesAppTestTask
+//
+//  Created by Nursultan Kabulov on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = UIColor.blue
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
